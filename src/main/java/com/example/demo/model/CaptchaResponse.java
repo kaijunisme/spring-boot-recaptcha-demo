c99333e612bfaa80412497d7bbd8c572d28ecbfb
kaijun <worktolife0829@gmail.com>
2021-12-23 14:26:02 +0800

    reCAPTCHA v3 HTML Example

--- src/main/java/com/example/demo/model/CaptchaResponse.java
+++ src/main/java/com/example/demo/model/CaptchaResponse.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class CaptchaResponse {
 
 	private boolean success;
+	private String score;
+	private String action;
 	private String challenge_ts;
 	private String hostname;
 
